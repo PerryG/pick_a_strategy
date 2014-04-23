82c33eb06b907038d3458a4291f92d370b25e464
Perry Green <perryagreen@gmail.com>
2014-04-23 12:26:57 -0400

    Find an interesting game

--- Scraping.py
+++ Scraping.py
@@ -1,3 +1,9 @@
+from pattern import web
+import random
+import requests
+import Game
+import Strategy
+
 # Returns a list of the names of top n players
 def top_n_players(n):
 	leaderboard = web.Element(requests.get('http://gokologs.drunkensailor.org/leaderboard/').text)
@@ -10,3 +16,35 @@ def get_log_links(p1, p2 = '', max_results = 100):
     url = template % (p1, p2, max_results)
     query = web.Element(requests.get(url).text)
     return [l.href for l in query.by_tag('a') if l.content == u'Log']
+
+# Picks a random game between 2 players in the top n where players had
+# different strategies according to strategy_class
+def random_interesting_game(n, strategy_class):
+	if not n >= 2:
+		raise Exception('Must choose from at least 2 players')
+
+	# pick 2 random players
+	players = top_n_players(n)
+	p1 = random.choice(players)
+	p2 = None
+	while p2 == None:
+		p2 = random.choice(players)
+		if p2 == p1:
+			p2 = None
+
+	# Get games for those players
+	links = random.shuffle(get_log_links(p1, p2))
+	found_game = False
+
+	# See if any of the links are interesting
+	for link in links:
+		game = Game(link, strategy_class)
+		if game.p1_strat != game.p2_strat:
+			found_game = True
+			break
+
+	# If none are interesting, start over
+	if not found_game:
+		return random_interesting_game(n, strategy_class)
+
+	return game
